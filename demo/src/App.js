d88d524970b4eaa53c3d25a79373ac82192b7680
Maksym Bykovskyy <maksym@bykovskyy.com>
2020-03-19 15:22:32 -0700

    Initial commit

---
+++ demo/src/App.js
@@ -0,0 +1,12 @@
+import { Boilerplate } from '@mbykovskyy/react-npm-component';
+import React from 'react';
+
+function App() {
+  console.log(Boilerplate);
+
+  return (
+    <Boilerplate></Boilerplate>
+  );
+}
+
+export default App;
